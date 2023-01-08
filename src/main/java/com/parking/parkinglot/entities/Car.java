d5c51974eafb9d9ac2d336054109e399754a3261
GaneaVictorSebastian <victorsebastian.ganea@ulbsibiu.ro>
2023-01-08 19:16:24 +0200

    Connected to the database

---
+++ src/main/java/com/parking/parkinglot/entities/Car.java
@@ -0,0 +1,49 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Car {
+    private Long id;
+    private String licensePlate;
+    private String parkingSpot;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public String getLicensePlate(){
+        return licensePlate;
+    }
+    public void setLicensePlate(String licensePlate){
+        this.licensePlate = licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    private user owner;
+
+    @ManyToOne
+    public user getOwner() {
+        return owner;
+    }
+
+    public void setOwner(user owner) {
+        this.owner = owner;
+    }
+}
